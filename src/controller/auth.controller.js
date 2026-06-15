bdf1e9a08a8fea60a0c27fd0e6d15ddf6916fc99
sarthak <231179030+MustbeSarthak@users.noreply.github.com>
2026-06-15 22:55:26 +0530

    Added Login

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -13,12 +13,12 @@ async function register(req,res){
         ]
     });
 
-    const hash = await bcrypt.hash(password, 10) // Hash password here with 10 rounds 
-
+    
     if(isUserAlreadyExist){
         return res.status(409).json({message: "User already exists"});
     }
-
+    
+    const hash = await bcrypt.hash(password, 10) // Hash password here with 10 rounds 
     const user = await UserSchema.create({
         username,
         password : hash, // User created with the hash password 
@@ -47,5 +47,67 @@ catch(err){
     res.status(500).json({message: "Internal Server Error", error: err.message});
 }
 }
+// Login user
+async function Login(req,res){
+    try{
+        const {username,email,password} = req.body;
+        if((!email && !username) || !password){
+            return res.status(400).json({
+                message: "Please provide your email and password"
+            })
+        }
 
-module.exports = {register}
+        // Check if user exists
+        const user = await UserSchema.findOne({
+            $or:[
+                {username},
+                {email}
+            ]
+        })
+
+        if(!user){
+            return res.status(401).json({
+                message:"Invalid Credentials"
+            })
+        }
+
+        // Password check 
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        // Token Assign
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },process.env.JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        )
+        //Assign Token
+        res.cookie("token", token)
+
+        //Respond 
+        res.status(200).json({
+            message: "Login Successfull",
+            token,
+            user:{
+                id: user._id,
+                username: user.username,
+                role: user.role
+            }
+        })   
+    }catch(err){
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error:err.message,
+        })
+    }
+}
+
+module.exports = {register , Login}
